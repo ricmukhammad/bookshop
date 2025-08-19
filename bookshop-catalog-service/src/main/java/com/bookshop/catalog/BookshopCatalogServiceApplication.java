635b451ea6bd6eb669cc5db50fec02352f56c889
Mukhammad Muminov <ricmukhammadalisherovich@gmail.com>
2025-08-19 21:20:43 +0200

    created catalog service

---
+++ bookshop-catalog-service/src/main/java/com/bookshop/catalog/BookshopCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bookshop.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookshopCatalogServiceApplication.class, args);
+    }
+
+}

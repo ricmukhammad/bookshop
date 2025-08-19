1459c88beeaf1f183ab875070d141545b01f7985
Mukhammad Muminov <ricmukhammadalisherovich@gmail.com>
2025-08-19 21:22:26 +0200

    created auth service

---
+++ bookshop-auth-service/src/main/java/com/bookshop/authservice/BookshopAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bookshop.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookshopAuthServiceApplication.class, args);
+    }
+
+}
---
+++ bookshop-auth-service/src/test/java/com/bookshop/authservice/BookshopAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookshop.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshopAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
